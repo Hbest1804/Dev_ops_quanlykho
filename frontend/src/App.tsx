757e47b530c41cb0d421503f72844844cf6cb127
orangecat <166352148+huyga1185@users.noreply.github.com>
2026-04-25 09:02:27 +0700

    Revert "feat: add AuthContext.tsx"
    This reverts commit 6b065e4bb1630849eb8e4617ccba695375ebeb3b.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,11 @@
 import Layout from "./component/Layout";
 import Dashboard from "./component/Dashboard";
 import { useState } from "react";
-import { useAuth } from "./contexts/AuthContext";
 
 export default function App() {
   const [currentView, setCurrentView] = useState('dashboard');
-  const { user, loading, logout } = useAuth();
-
-  if (loading) {
-    return <div className="min-h-screen flex items-center justify-center font-medium text-slate-500">Đang tải dữ liệu hệ thống...</div>;
-  }
-
   return (  
-    <Layout currentView={currentView} setView={setCurrentView} onLogout={logout} >
+    <Layout currentView={currentView} setView={setCurrentView} onLogout={() => {}} >
       {currentView === 'dashboard' && <Dashboard />}
     </Layout>
   );
