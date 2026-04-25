08820352d260c9eb624559ff74470bce51a8ffef
huyga1185 <huylay1054@gmail.com>
2026-04-25 09:00:23 +0700

    feat(frontend): add login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster position='top-right' />
+    <AuthProvider>
+      <App />
+      <Toaster position='top-right' />
+    </AuthProvider>
   </StrictMode>,
 )
